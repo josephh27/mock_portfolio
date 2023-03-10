3e6d66ba12d9f3464dff1ec65bed0e985e7451d4
josephh27 <josephjasonsbuhain@iskolarngbayan.pup.edu.ph>
2023-03-10 23:22:02 +0800

    finished the complete layout and animations.

---
+++ index.js
@@ -0,0 +1,54 @@
+gsap.registerPlugin(ScrollTrigger);
+let tl = gsap.timeline();
+
+tl.from('.content', {
+    y: '-30%',
+    opacity: 0,
+    duration: 2,
+    ease: Power4.easeOut
+})
+
+tl.from('.stagger1', {
+    opacity: 0,
+    y: -50, 
+    stagger: .3,
+    ease: Power4.easeOut,
+    duration: 2
+}, "-=1.5")
+
+tl.from('.hero-design', {
+    opacity: 0,
+    y: 50,  
+    ease: Power4.easeOut, 
+    duration: 1
+}, "-=2")
+
+
+gsap.from(".square-anim", {
+    stagger: .3,
+    scale: 0.1,
+    duration: 1,
+    ease: Back.easeOut.config(1.7)
+})
+
+gsap.from(".transition2", {
+    scrollTrigger: {
+        trigger: '.transition2',
+        start: "top bottom"
+    },
+    y: 50,
+    opacity: 0,
+    duration: 1.2,
+    stagger: .3
+})
+
+gsap.from(".transition3", {
+    scrollTrigger: {
+        trigger: '.transition3',
+        start: "top center"
+    },
+    y: 50,
+    opacity: 0,
+    duration: 1.2,
+    stagger: 1
+})
